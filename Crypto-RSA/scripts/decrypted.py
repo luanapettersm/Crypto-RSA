78fb2e03843c94be9a393d32acd3b18d52fdafed
luanapettersm <91229163+luanapettersm@users.noreply.github.com>
2023-07-04 13:50:14 -0300

    Add files via upload

--- Crypto-RSA/scripts/decrypted.py
+++ Crypto-RSA/scripts/decrypted.py
@@ -2,26 +2,24 @@ import text_chunk
 import base64
 
 print('Necessário inserir o caminho completo para os arquivos.')
-keyFile = input('Arquivo com chave para criptografar: ')
-source = input('Arquivo descriptografado: ')
-output = input('Arquivo para preencher com o conteúdo descriptografado: ')
+# keyFile = input('Arquivo com chave para criptografar: ')
+# source = input('Arquivo descriptografado: ')
+# output = input('Arquivo para preencher com o conteúdo descriptografado: ')
 
-keyFile = open(keyFile)
-source = open(source).readlines()
-output = open(output, 'w')
-
-keys = keyFile.readlines()
+keys = open('./files/public.txt').read()
+source = open('./files/output.txt').read()
+output = open('./files/outputDecrypted.txt', 'w')
 
 mod = int(keys[0])
 key = int(keys[1])
 
 resultText = ''
 
-for line in source:
+result = base64.b64decode(bytes(source.encode())).decode()
+
+for line in result:
     originalChunk = pow(int(line), key, mod)
     chunk = text_chunk._int_to_str(originalChunk)
     resultText += chunk
 
-result = base64.b64decode(bytes(resultText.encode())).decode()
-    
-output.write(str(result))
+output.write(str(resultText))
--- Crypto-RSA/scripts/encrypted.py
+++ Crypto-RSA/scripts/encrypted.py
@@ -2,39 +2,39 @@ import base64
 import text_chunk
 
 print('Necessário inserir o caminho completo para os arquivos.')
-keyFile = input('Arquivo com chave para criptografar: ')
-source = input('Arquivo criptografado: ')
-output = input('Arquivo para preencher com o conteúdo criptografado: ')
+# keyFile = input('Arquivo com chave para criptografar: ')
+# source = input('Arquivo criptografado: ')
+# output = input('Arquivo para preencher com o conteúdo criptografado: ')
 
-keyFile = open(keyFile)
-source = open(source)
-output = open(output, 'w')
+# Abrir arquivos
+keyFile = open('./files/private.txt')
+source = open('./files/primeList.txt')
+output = open('./files/output.txt', 'w')
 
-keys = keyFile.readlines()
+# Lido o keyFiles
+keys = keyFile.read()
 
 mod = int(keys[0])
 key = int(keys[1])
 
-text = source.readlines()
-resultText = ''
-
-for line in resultText:
-    resultText += line
+# Lido o source
+resultText = source.read()
 
 print('String para criptografar: '+resultText)
 
 chunkSize = text_chunk.block_size(mod)
 print('Defined a ' + str(chunkSize) +' chunkSize')
 
-encoded = base64.b64encode(bytes(resultText.encode())).decode()
-
-chunks = [encoded[i:i+chunkSize] for i in range(0, len(encoded), chunkSize)]
+chunks = [resultText[i:i+chunkSize] for i in range(0, len(resultText), chunkSize)]
 print(chunks)
+
+result = ''
 
 for chunk in chunks:
     
     originalChunk = text_chunk.TextChunk(chunk).int_value()
 
-    result = pow(originalChunk, key, mod)
-    
-    output.write(str(result))
+    result += pow(originalChunk, key, mod)   
+
+encoded = base64.b64encode(bytes(resultText.encode())).decode()
+output.write(str(encoded))
