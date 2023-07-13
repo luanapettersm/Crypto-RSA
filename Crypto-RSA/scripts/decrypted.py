2d9680d211404cede52cdc4de05077111aebe924
Luana Petters <luana.petters@byseven.com.br>
2023-07-12 21:07:12 -0300

    Alteraçõs no código para usar o private, public e primeList deisponibilzados pelo professor; Alterações no encrypted.py e decrypted.py para uso do pow e correção das condições.

--- Crypto-RSA/scripts/decrypted.py
+++ Crypto-RSA/scripts/decrypted.py
@@ -1,18 +1,15 @@
 import text_chunk
 import base64
 
-keys = open('./files/public.txt').readlines()
-source = open('./files/output.txt').read()
+keys = open('./files/private.txt').readlines()
+source = open('./files/output.txt').readlines()
 output = open('./files/outputDecrypted.txt', 'w')
 
-chunk = ''
+result_string = ''
 
-result = base64.b64decode(bytes(source.encode())).decode()
+for line in source:
+    decoded = pow(int(line), int(keys[1]), int(keys[0]))
+    result_string += str(text_chunk.TextChunk(decoded))
 
-for line in result.split(','):
-    if line == '\n' or line == '':
-        chunk += '\n'
-    else:
-        chunk += text_chunk._int_to_str(int(line))
-
-output.write(str(chunk))
+decoded_string = base64.b64decode(bytes(result_string, "utf-8")).decode("utf-8", "ignore")
+output.write(decoded_string)
--- Crypto-RSA/scripts/encrypted.py
+++ Crypto-RSA/scripts/encrypted.py
@@ -1,26 +1,27 @@
 import base64
 import text_chunk
 
-keys = open('./files/private.txt').readlines()
+keys = open('./files/public.txt').readlines()
 source = open('./files/primeList.txt')
 output = open('./files/output.txt', 'w')
 
 resultText = source.read()
 
+encoded_source = base64.b64encode(bytes(resultText, "utf-8"))
+
 print('String para criptografar: '+resultText)
 
-chunkSize = text_chunk.block_size(len(resultText))
+chunkSize = text_chunk.block_size(int(keys[0]))
 print('Defined a ' + str(chunkSize) +' chunkSize')
 
-chunks = [resultText[i:i+chunkSize] for i in range(0, len(resultText), chunkSize)]
+chunks = [encoded_source[i:i+chunkSize] for i in range(0, len(encoded_source), chunkSize)]
 print(chunks)
 
 originalChunk = ''
 
 for chunk in chunks:
-    
-    originalChunk += str(text_chunk.TextChunk(chunk).int_value())
-    originalChunk += ','
-
-encoded = base64.b64encode(bytes(originalChunk.encode())).decode()
-output.write(str(encoded))
+    original_chunk = text_chunk.TextChunk(chunk.decode('utf-8')).int_value()
+    encoded = pow(original_chunk, int(keys[1]), int(keys[0]))
+    output.write(str(encoded))
+    output.write('\n')
+output.close()
